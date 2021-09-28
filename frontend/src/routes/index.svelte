d4c86b3f3fb4985c2e818795f4a0e04ee7bd4cff
thanet-s <cho.112543@gmail.com>
2021-09-29 01:15:14 +0700

    setup project

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>สวัสดีชาวโลก</title>
+</svelte:head>
+
+<script>
+    let count = 0;
+    const buttonHandle = () => {
+      count = count+1  
+    };
+</script>
+
+<h1>สวัสดีชาวโลก!</h1>
+
+<button on:click={buttonHandle}>click to increate</button>
+
+<p>Count is {count}</p>

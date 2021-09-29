785e145016b6685a3f23c36af47b9f5c51df5281
thanet-s <cho.112543@gmail.com>
2021-09-29 22:05:29 +0700

    set adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,7 +5,6 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapter({
-			// default options are shown
 			out: 'build',
 			precompress: false,
 			env: {

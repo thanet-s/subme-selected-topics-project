02448fdd8c7e11e29d34bd3ba7e130045ccac910
thanet-s <cho.112543@gmail.com>
2021-10-12 17:13:47 +0700

    set new frontend

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,19 +1,19 @@
-<svelte:head>
-    <title>Home</title>
-</svelte:head>
-
 <script>
-  import { Button, Modal } from "sveltestrap/src";
-  let isOpen = false;
-  const toggle = () => (isOpen = !isOpen);
+	import { Button, Modal } from 'sveltestrap/src';
+	let isOpen = false;
+	const toggle = () => (isOpen = !isOpen);
 </script>
+
+<svelte:head>
+	<title>Subme</title>
+</svelte:head>
 
 <Button color="primary" on:click={toggle}>Hello World!</Button>
 <Modal body {isOpen} {toggle} header="Hello World!">
-  <p>There's a song that we're singing. Come on</p>
-  <img
-    src="https://i.ytimg.com/vi/NUJIRujygvY/hqdefault.jpg"
-    alt="Come on Get Happy"
-    class="img-fluid"
-  />
+	<p>There's a song that we're singing. Come on</p>
+	<img
+		src="https://i.ytimg.com/vi/NUJIRujygvY/hqdefault.jpg"
+		alt="Come on Get Happy"
+		class="img-fluid"
+	/>
 </Modal>
